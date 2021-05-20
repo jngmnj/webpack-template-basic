3b0bad315f8e2502039ce7de3acb93cc7d2d81ec
jngminj <exorbitance@naver.com>
2021-05-21 00:31:27 +0900

    webpack sampler

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path')
+const HtmlPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+
+module.exports = {
+  // parcel main.js
+  // 파일을 읽어들이기 시작하는 진입점 설정
+  entry: './js/main.js',
+
+  // 결과물(번들)을 반환하는 설정
+  output: {
+    // path: path.resolve(__dirname, 'dist'),
+    // filename: 'main.js',
+    clean: true
+  },
+
+
+  module: {
+    rules: [
+      {
+        test: /\.s?css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'postcss-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.js$/,
+        use: [
+          'babel-loader'
+        ]
+      }
+    ]
+  },
+
+  // 번들링 후 결과물의 처리방식 등 다양한 플러그인들을 설정
+  plugins: [
+    new HtmlPlugin({
+      template: './index.html'
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: 'static' }
+      ]
+    })
+  ],
+
+  devServer: {
+    host: 'localhost'
+  }
+}
+
+// path는 절대경로필요해서 nodejs에서 
+// 전역변수__dirname(현재파일이 있는 그 경로)와 dist를 합쳐줌
+// dist에 있는 main.js를 번들로 만들어서 내어줌
